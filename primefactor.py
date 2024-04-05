615881af149d4c5a7250be2fb234f4e401236bd2
Ephraim67 <jonathaneuphrem@gmail.com>
2024-04-05 14:53:08 -0400

    Add for more changes

--- primefactor.py
+++ primefactor.py
@@ -2,16 +2,26 @@
 
 import sys
 
-def prime_fact(n):
-    factors = []
-    divisor = 2
-    while divisor <= n:
-        if n % divisor == 0:
-            factors.append(divisor)
-            n = n // divisor
-        else:
-            divisor += 1
-    return factors
+def factorize(num):
+
+    factor1 = 2
+    while (num % factor1):
+        if (factor1 <= num):
+            factor1 += 1
+
+    factor2 = num // factor1
+    return (factor2, factor1)
+
+# def prime_fact(n):
+#     factors = []
+#     divisor = 2
+#     while divisor <= n:
+#         if n % divisor == 0:
+#             factors.append(divisor)
+#             n = n // divisor
+#         else:
+#             divisor += 1
+#     return factors
 
 def main(filename):
     with open(filename, 'r') as file:
@@ -19,7 +29,7 @@ def main(filename):
         
     for num in numbers:
         num = int(num)
-        factor_pairs = prime_fact(num)
+        factor_pairs = factorize(num)
         if factor_pairs:
             for pair in factor_pairs:
                 print(f"{num} = {pair}*{num//pair}")
@@ -28,7 +38,7 @@ def main(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: factors filename")
+        print(f"Wrong Usage: {sys.argv[0]} <file_path>")
         sys.exit(1)
     filename = sys.argv[1]
     main(filename)
