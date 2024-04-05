fae3811c2834a6578be385e2e2a1030bc8af5133
Ephraim67 <jonathaneuphrem@gmail.com>
2024-04-05 03:25:25 -0400

    A secont new test

--- factors_function.py
+++ factors_function.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-import sys
-import ctypes
-from resource import getrusage as resource_usage, RUSAGE_SELF
-from time import time as timestamp
+
+"""factorize given set of numbers in a file to product of two small numbers"""
+from sys import argv
 
 
-def unix_time(function):
-    '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
-    You can calculate the amount of used CPU-time used by your
-    function/callable by summing `user` and `sys`. `real` is just like the wall
-    clock.
-    Note that `sys` and `user`'s resolutions are limited by the resolution of
-    the operating system's software clock (check `man 7 time` for more
-    details).
-    '''
-    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
-    function()
-    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
+def factorize(number_check):
+    """"find two small numbers that multiply to give a given number"""
+    x = 2
 
-    return "\nreal: {}\nuser: {}\nsys: {}".format(
-        end_time - start_time,
-        end_resources.ru_utime - start_resources.ru_utime,
-        end_resources.ru_stime - start_resources.ru_stime)
+    if number_check < 2:
+        return
+    
+    while number_check % i:
+        i += 1
+    print("{:.0f}={:.0f}*{:.0f}".format(number_check, number_check / x, x))
 
+if len(argv) != 2:
+    exit()
 
-def print_factors():
-    fun = ctypes.CDLL("./lib_factors_functions.so")
-    fun.trial_division.argtypes = [ctypes.c_long]
-    with open(sys.argv[1], 'r') as prime:
-        line = prime.readline()
-        while line != '':
-            n = int(line)
-            fun.trial_division(n)
-            line = prime.readline()
+try:
+    with open(argv[1]) as file:
+        line = file.readline()
+
+        while line != "":
+            number_check = int(line.split('\n')[0])
+            factorize(number_check)
+            line = file.readline()
+except:
+    pass
